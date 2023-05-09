4eb6ab056b30022a71e90ff715f7f1099ab1574d
wrasdf <452784+wrasdf@users.noreply.github.com>
2023-05-09 14:43:20 +1000

    Test

--- v1beta1/local.tf
+++ v1beta1/local.tf
@@ -1,11 +1,12 @@
 locals {
 
   default_tags = {
-    team              = "Platform Kompute"
-    created_by        = "ops-kube-crossplane-terraform"
-    managed_by        = "crossplane terraform-provider"
-    service_name      = "terraform dynamodb"
-    deployment_method = "Terraform"    
+    team                  = "Platform Kompute"
+    created_by            = "ops-kube-crossplane-terraform"
+    managed_by            = "crossplane terraform-provider"
+    service_name          = "terraform dynamodb"
+    deployment_method     = "Terraform"
+    crossplane.io/paused  = "true"
   }
   
 }
